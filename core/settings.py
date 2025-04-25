c14115c60cdbfd7dc725ffda4ed07cdec5e690ef
Harshit Ghosh <harshitghosh6@gmail.com>
2025-04-25 18:03:42 +0530

    event_model_creation

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'reminders',
+    'django_q',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+Q_CLUSTER = {
+    'name': 'BirthdayAppQueue',
+    'workers': 2,
+    'retry': 120,
+    'timeout': 60,
+    'queue_limit': 50,
+    'bulk': 10,
+    'orm': 'default',
+}
--- reminders/models.py
+++ reminders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Event(models.Model):
+    EVENT_TYPES=[
+        ('birthday', 'Birthday'),
+        ('anniversary', 'Anniversary'),
+        ('other', 'Other')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='events')
+    name=models.CharField(max_length=100)
+    event_type=models.CharField(max_length=20, choices=EVENT_TYPES)
+    data=models.DateField()
+    remind_days_before=models.IntegerField(default=1)
+    message=models.TextField(blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.name}'s{self.get_event_type_display()}on {self.data}"
