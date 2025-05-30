d7ef6cbf7bc2d92f044022e584e9da747d9f8358
Harshit Ghosh <harshitghosh6@gmail.com>
2025-05-30 20:16:56 +0530

    Update urls.py

--- reminders/urls.py
+++ reminders/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('update/<int:event_id>/', views.edit_event, name='event_update'),
     path('delete/<int:event_id>/', views.delete_event, name='event_delete'),
     path('send-daily-reminders/', trigger_send_reminders, name='send_daily_reminders'),
+    path('media/delete/<int:event_id>/', views.delete_event_media, name='delete_event_media'),
+
 ]
